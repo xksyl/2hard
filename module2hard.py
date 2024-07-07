5d2e5bf7efe6d3087c876999026cf42e8e4c4bc1
xksyl <egorka2004212@gmail.com>
2024-07-07 13:40:26 +0500

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,12 @@
+input_numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+for i in input_numbers:
+    empty_string = ""
+    for p in range(1, 20):
+        for j in range(p + 1, 20):
+            if i % (p + j) == 0:
+                empty_string = empty_string + ''.join(map(str, [p, j]))
+            else:
+                continue
+
+    print(i, "-", empty_string)
